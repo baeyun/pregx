159a5f6fdf12da378a2cb7fff0e212c710cd56b0
bukharim96 <bukharim96@gmail.com>
2018-03-20 04:19:53 +0200

    config.global to config.matchAll and set default: true

--- src/regex-parser.js
+++ src/regex-parser.js
@@ -11,10 +11,10 @@ export default (str, pattern, config) => {
 	if (!pattern && typeof pattern !== 'string')
 		throw new TypeError('The second argument is not a string. Please provide a string value.')
 
-	let flags = []
+	let flags = ['g'] // match all by default
 
 	if (config && config.constructor == Object) {
-		let { global, ignoreCase, multiLine, matchUnicode, stickyMatch, exclude, strict } = config
+		let { matchAll, ignoreCase, multiLine, matchUnicode, stickyMatch, exclude, strict } = config
 
 		strict = config.strict
 
@@ -26,7 +26,7 @@ export default (str, pattern, config) => {
 		// 	pattern = `(?![${exclude.join('|')}])${pattern}`
 
 		// Flags
-		if (global === true)       flags.push('g')
+		if (matchAll === false)    flags.splice(0, 1)
 		if (ignoreCase === true)   flags.push('i')
 		if (multiLine === true)    flags.push('m')
 		if (matchUnicode === true) flags.push('u')
