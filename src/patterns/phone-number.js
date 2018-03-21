46bf1da0d110ffe2fe149f8890b9966b9a8b76a8
bukharim96 <bukharim96@gmail.com>
2018-03-21 23:38:33 +0200

    Fixed and tested the phone-number pattern

--- src/patterns/phone-number.js
+++ src/patterns/phone-number.js
@@ -12,6 +12,6 @@ import regexParser from '../regex-parser'
 export default (str, config) =>
 	regexParser(
 		str,
-		'(([0-9]{1})*[- .(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4})+',
+		'\\b(([0-9]{1})*[- .(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4})+\\b',
 		config || null
 	)
